d58b8dfa59a5e8365590b99a82b273cdcdc15175
Victor Lawrence <VictorLawrenceCode@gmail.com>
2018-09-11 15:56:56 -0400

    cleaned up code and added an alias for MakePageLink called MakeLinkTo

--- src/components/Link.component.jsx
+++ src/components/Link.component.jsx
@@ -11,6 +11,8 @@ import { history } from "../history"
 
 export const makePageLink = ( route ) => (props) => ( <Link {...props} to={route}/> )
 
+export const makeLinkTo = ( route ) => (props) => ( <Link {...props} to={route}/> )
+
 export const Link = ({ className, type, to, isActive, elm, ...props })=>{
 	const modifier = {}
 	modifier['active'] = isActive
@@ -19,8 +21,6 @@ export const Link = ({ className, type, to, isActive, elm, ...props })=>{
 	const clickHandler = (e)=>{ 
 		e.preventDefault() 
 		if(to){
-			// console.log("Would go to:", to)
-			// store.dispatch({type:'[history] set-pathname',payload:to})
 			history.push(to)
 		}else{
 			console.log("No Link To!")
